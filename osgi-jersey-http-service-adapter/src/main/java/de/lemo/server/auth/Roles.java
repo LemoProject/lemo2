75b3a3795cbcfe35d034f15eb141c83336a9fe4d
Leonard Kappe <kappe@htw-berlin.de>
2015-02-28 02:41:43 +0100

    Add role based auth

---
+++ osgi-jersey-http-service-adapter/src/main/java/de/lemo/server/auth/Roles.java
@@ -0,0 +1,7 @@
+package de.lemo.server.auth;
+
+public enum Roles {
+
+	ADMIN, USER, ;
+
+}
---
+++ osgi-jersey-http-service-adapter/src/main/java/de/lemo/server/auth/SecurityFilter.java
@@ -0,0 +1,30 @@
+package de.lemo.server.auth;
+
+import java.io.IOException;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class SecurityFilter implements ContainerRequestFilter {
+
+	private static final Logger logger = LoggerFactory.getLogger(SecurityFilter.class);
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+
+		HmacSecurityContext securityContext = new HmacSecurityContext(null, null, false);
+
+		requestContext.setSecurityContext(securityContext);
+
+		logger.info("############## FILTER");
+	}
+
+}
