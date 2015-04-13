130e042fc64e32f00ee8d811d98c1616eb1a08b9
Leonard Kappe <kappe@htw-berlin.de>
2015-04-13 05:49:15 +0200

    Remove static resource mapping

--- de.lemo.rest.adapter/src/main/java/de/lemo/rest/adapter/RestResourceAdapter.java
+++ de.lemo.rest.adapter/src/main/java/de/lemo/rest/adapter/RestResourceAdapter.java
@@ -2,12 +2,9 @@ package de.lemo.rest.adapter;
 
 import java.util.ArrayList;
 import java.util.Dictionary;
-import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
-import java.util.Map;
 
-import javax.inject.Singleton;
 import javax.servlet.Servlet;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -20,23 +17,14 @@ import org.apache.felix.ipojo.annotations.Invalidate;
 import org.apache.felix.ipojo.annotations.Unbind;
 import org.apache.felix.ipojo.annotations.Validate;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
-import org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature;
 import org.glassfish.jersey.servlet.ServletContainer;
-import org.ops4j.pax.web.extender.whiteboard.ResourceMapping;
-import org.ops4j.pax.web.extender.whiteboard.runtime.DefaultResourceMapping;
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.http.HttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.lemo.rest.api.WebResource;
-import freemarker.cache.ClassTemplateLoader;
-import freemarker.cache.WebappTemplateLoader;
-import freemarker.template.Configuration;
 
 @SuppressWarnings("rawtypes")
 @Component
@@ -46,16 +34,12 @@ public class RestResourceAdapter {
 	private static final Logger logger = LoggerFactory.getLogger(RestResourceAdapter.class);
 
 	private static final String applicationName = "lemo";
-	private static final String assetsName = "assets";
 	private static final String applicationBasePath = "/lemo";
-	private static final String assetPath = applicationBasePath + "/assets";
 
 	private ServletContainer servletContainer;
-	private Resource2HttpContext resourceContext;
 
 	private ServiceRegistration servletContainerRegistration;
-	private ServiceRegistration resourceContextRegistration;
-	private ServiceRegistration resourceMappingRegistration;
+
 
 	@Context
 	private BundleContext context;
@@ -63,14 +47,13 @@ public class RestResourceAdapter {
 	List<WebResource> resourceInstances = new ArrayList<>();
 
 	@Bind(aggregate = true, optional = true)
-	public synchronized void bindResources(WebResource resource) {
+	public synchronized void bindResources(WebResource webResource) {
 
-		boolean isResource = resource.getClass().isAnnotationPresent(Path.class);
-		logger.warn("XXXXXXX " + resource.getClass());
-		boolean isSingleton = resource.getClass().isAnnotationPresent(Singleton.class);
+		boolean isResource = webResource.getClass().isAnnotationPresent(Path.class);
+		// boolean isSingleton = webResource.getClass().isAnnotationPresent(Singleton.class);
 
 		if (isResource) {
-			resourceInstances.add(resource);
+			resourceInstances.add(webResource);
 			reloadApplication();
 		}
 
@@ -110,28 +93,18 @@ public class RestResourceAdapter {
 	@Validate
 	public void activate() {
 		initServletContainer(context);
-		initResourceMapping(context);
 		reloadApplication();
 	}
 
 	@Invalidate
 	protected void deactivate() {
 		tryUnregister(servletContainerRegistration);
-		tryUnregister(resourceMappingRegistration);
-		tryUnregister(resourceContextRegistration);
 	}
 
 	private void initServletContainer(BundleContext applicationContext) {
 		Dictionary<String, Object> webappServletProperties = createServletProperties(applicationName, applicationBasePath);
 		servletContainer = new ServletContainer();
-
 		servletContainerRegistration = applicationContext.registerService(Servlet.class.getName(), servletContainer, webappServletProperties);
-	}
-
-	private void initResourceMapping(BundleContext applicationContext) {
-		resourceContext = new Resource2HttpContext();
-		resourceContextRegistration = applicationContext.registerService(HttpContext.class.getName(), resourceContext, null);
-		resourceMappingRegistration = registerResourceMapping(applicationContext, assetsName, assetPath);
 	}
 
 	private synchronized void reloadApplication() {
@@ -148,38 +121,10 @@ public class RestResourceAdapter {
 		resourceConfig.packages("de.lemo.server.auth");
 		resourceConfig.registerInstances(new Foo(this));
 
-		// freemarker
-		Map<String, Object> properties = new HashMap<>();
-		properties.put(FreemarkerMvcFeature.TEMPLATES_BASE_PATH, "/templates");
-		properties.put(FreemarkerMvcFeature.TEMPLATE_OBJECT_FACTORY, createFreemarkerConfiguration());
-		resourceConfig.addProperties(properties);
-		resourceConfig.register(FreemarkerMvcFeature.class);
-
 		for (WebResource resourceInstance : resourceInstances) {
-			resourceConfig.register(resourceInstance);
+			resourceConfig.registerInstances(resourceInstance);
 		}
 		servletContainer.reload(resourceConfig);
-
-		// reload resource context
-		Map<String, Bundle> pluginPathMapping = new HashMap<>();
-		// for (WebResource resource2 : resourceInstances) {
-		// Bundle bundle = FrameworkUtil.getBundle(resource.getClass());
-		// String path = "/" +
-		// trim(resource.getClass().getAnnotation(Path.class).value(), '/');
-		// pluginPathMapping.put(path, bundle);
-		// }
-		resourceContext.reload(pluginPathMapping);
-	}
-
-	private Configuration createFreemarkerConfiguration() {
-		Configuration configuration = new Configuration();
-		// BeansWrapper beansWrapper = BeansWrapper.getDefaultInstance();
-
-		configuration.setTemplateLoader(new ClassTemplateLoader(getClass(), "/"));
- 
-		 
-		
-		return configuration;
 	}
 
 	private boolean tryUnregister(ServiceRegistration registration) {
@@ -194,27 +139,13 @@ public class RestResourceAdapter {
 		return true;
 	}
 
-	private ServiceRegistration registerResourceMapping(BundleContext applicationContext, String name, String path) {
-		DefaultResourceMapping resourceMapping = new DefaultResourceMapping();
-		resourceMapping.setAlias(path);
-		resourceMapping.setPath(name);
-		ServiceRegistration registerService = applicationContext.registerService(ResourceMapping.class.getName(), resourceMapping, null);
-		return registerService;
-	}
-
 	private Dictionary<String, Object> createServletProperties(String name, String path) {
 		Dictionary<String, Object> props = new Hashtable<>();
 
 		// osgi http service
-		props.put(HttpConstants.SERVLET_NAME, name); // TODO should work without "init" but doesn't (?)
+		props.put(HttpConstants.SERVLET_NAME, name);
 		props.put(HttpConstants.INIT_PREFIX + HttpConstants.SERVLET_NAME, name);
 		props.put(HttpConstants.ALIAS, path);
-
-		// jersey
-		props.put(ServerProperties.APPLICATION_NAME, name);
-		// props.put(ServerProperties.METAINF_SERVICES_LOOKUP_DISABLE, "true");
-		// props.put(ServerProperties.FEATURE_AUTO_DISCOVERY_DISABLE, "true");
-		// props.put(ServerProperties.WADL_FEATURE_DISABLE, "true");
 
 		return props;
 	}
