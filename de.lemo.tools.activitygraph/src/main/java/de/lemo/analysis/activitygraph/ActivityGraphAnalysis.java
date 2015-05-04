8ea391b6a7dc57b896f1c15edb912710624dccae
Kluesener <mkluesener@gmx.net>
2015-05-04 12:19:13 +0200

    Integrated activity graph.

--- de.lemo.tools.activitygraph/src/main/java/de/lemo/analysis/activitygraph/ActivityGraphAnalysis.java
+++ de.lemo.tools.activitygraph/src/main/java/de/lemo/analysis/activitygraph/ActivityGraphAnalysis.java
@@ -1,4 +1,4 @@
-package de.lemo.analysis.circlegraph;
+package de.lemo.analysis.activitygraph;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,30 +20,30 @@ import de.lemo.tools.api.AnalyticsTool;
 @Provides
 @Instantiate
 @Singleton
-@Path("tools/circlegraph")
-public class CircleGraphAnalysis implements WebResource, AnalyticsTool {
+@Path("tools/activitygraph")
+public class ActivityGraphAnalysis implements WebResource, AnalyticsTool {
 
-	private static final Logger logger = LoggerFactory.getLogger(CircleGraphAnalysis.class);
+	private static final Logger logger = LoggerFactory.getLogger(ActivityGraphAnalysis.class);
 
 	@ServiceProperty(name = "lemo.tool.id")
-	private String id = "circlegraph";
+	private String id = "activitygraph";
 
 	@ServiceProperty(name = "lemo.tool.name")
-	private String name = "Circle Graph";
+	private String name = "Activity Graph";
 
 	@ServiceProperty(name = "lemo.tool.description.short")
-	private String descriptionShort = "Zeigt Navigationsschritte der Nutzer zwischen einzelnen Lernobjekten.";
+	private String descriptionShort = "Shows the users' navigational steps between learning objects within the course.";
 
 	@ServiceProperty(name = "lemo.tool.description.long")
-	private String descriptionLong = "Mit der Analyse „Circle Graph“ können Sie einen Einblick in das Navigationsverhalten der Nutzer erhalten, "
-			+ "insbesondere in die Reihenfolge, in der Studierende die Lernobjekte aufrufen.";
+	private String descriptionLong = "Shows the users' navigational steps between learning objects within the course.";
 
 	@ServiceProperty(name = "lemo.tool.scripts")
 	private final List<String> scripts;
 	{
 		scripts = new ArrayList<>();
-		scripts.add("js/circlegraph.js");
-		scripts.add("js/fake_data.js");
+		scripts.add("js/ActivityGraph.js");
+		scripts.add("js/ActivityGraphData.js");
+		scripts.add("js/jquery.tipsy.js");
 	}
 
 	@ServiceProperty(name = "lemo.tool.image.icon.monochrome")
