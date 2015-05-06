b606aa40473902f12c8943597eeb638e67b2f757
Kluesener <mkluesener@gmx.net>
2015-05-06 12:44:16 +0200

    Integration of performance cumulative.

--- de.lemo.tools.performancecumulative/src/main/java/de/lemo/analysis/performancecumulative/PerformanceCumulativeAnalysis.java
+++ de.lemo.tools.performancecumulative/src/main/java/de/lemo/analysis/performancecumulative/PerformanceCumulativeAnalysis.java
@@ -1,4 +1,4 @@
-package de.lemo.analysis.circlegraph;
+package de.lemo.analysis.performancecumulative;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,30 +20,30 @@ import de.lemo.tools.api.AnalyticsTool;
 @Provides
 @Instantiate
 @Singleton
-@Path("tools/circlegraph")
-public class CircleGraphAnalysis implements WebResource, AnalyticsTool {
+@Path("tools/performancecumulative")
+public class PerformanceCumulativeAnalysis implements WebResource, AnalyticsTool {
 
-	private static final Logger logger = LoggerFactory.getLogger(CircleGraphAnalysis.class);
+	private static final Logger logger = LoggerFactory.getLogger(PerformanceCumulativeAnalysis.class);
 
 	@ServiceProperty(name = "lemo.tool.id")
-	private String id = "circlegraph";
+	private String id = "performancecumulative";
 
 	@ServiceProperty(name = "lemo.tool.name")
-	private String name = "Circle Graph";
+	private String name = "Performance Cumulative";
 
 	@ServiceProperty(name = "lemo.tool.description.short")
-	private String descriptionShort = "Zeigt Navigationsschritte der Nutzer zwischen einzelnen Lernobjekten.";
+	private String descriptionShort = "Shows the distribution of grades for each graded learning object.";
 
 	@ServiceProperty(name = "lemo.tool.description.long")
-	private String descriptionLong = "Mit der Analyse „Circle Graph“ können Sie einen Einblick in das Navigationsverhalten der Nutzer erhalten, "
-			+ "insbesondere in die Reihenfolge, in der Studierende die Lernobjekte aufrufen.";
+	private String descriptionLong = "Shows the distribution of grades for each graded learning object.";
 
 	@ServiceProperty(name = "lemo.tool.scripts")
 	private final List<String> scripts;
 	{
 		scripts = new ArrayList<>();
-		scripts.add("js/circlegraph.js");
-		scripts.add("js/fake_data.js");
+		scripts.add("js/BoxPlot.js");
+		scripts.add("js/PerformanceCumulativeData.js");
+		scripts.add("js/BoxPlot_Lib.js");
 	}
 
 	@ServiceProperty(name = "lemo.tool.image.icon.monochrome")
